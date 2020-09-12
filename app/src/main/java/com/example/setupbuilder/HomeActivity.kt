12161715546ed5df8c5deb262e1b1fb782cb11c5
Monike Freitas <monike.ftsousa@gmail.com>
2020-09-12 12:27:16 -0400

    Splash Screen com autenticação

--- app/src/main/java/com/example/setupbuilder/HomeActivity.kt
+++ app/src/main/java/com/example/setupbuilder/HomeActivity.kt
@@ -1,11 +1,21 @@
 package com.example.setupbuilder
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.home_activity.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
+
+        sign_out.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/setupbuilder/SplashActivity.kt
+++ app/src/main/java/com/example/setupbuilder/SplashActivity.kt
@@ -1,12 +1,29 @@
 package com.example.setupbuilder
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_activity)
+
+        var user = FirebaseAuth.getInstance().currentUser
+
+        if(user != null) {
+            Toast.makeText(this, "User ${user.email}", Toast.LENGTH_LONG).show()
+                    var intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+        } else {
+            var intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+        }
 
     }
 }
