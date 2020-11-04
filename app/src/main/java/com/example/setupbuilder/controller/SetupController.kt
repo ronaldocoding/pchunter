a776c44cd2adab4fec11fa7a6d5ebd45f56b6c07
Monike Freitas <monike.ftsousa@gmail.com>
2020-11-04 09:32:28 -0400

    fix: correção da listagem de setups

--- app/src/main/java/com/example/setupbuilder/controller/SetupController.kt
+++ app/src/main/java/com/example/setupbuilder/controller/SetupController.kt
@@ -2,19 +2,21 @@ package com.example.setupbuilder.controller
 
 import com.example.setupbuilder.model.Setup
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
 
 class SetupController {
+    val userUid = UserController().getUID()
     val setupFirebase = FirebaseFirestore.getInstance().collection("setup")
 
     public fun listSetupsByTime(order: String): Task<QuerySnapshot> {
         var dir = Query.Direction.ASCENDING
         if(order.equals("desc"))
             dir = Query.Direction.DESCENDING
-        return setupFirebase.orderBy("timestamp", dir).get()
+        return setupFirebase.whereEqualTo("userUid", userUid.toString()).orderBy("timestamp", dir).get()
     }
 
     public fun add(setup: Setup): Task<DocumentReference> {
